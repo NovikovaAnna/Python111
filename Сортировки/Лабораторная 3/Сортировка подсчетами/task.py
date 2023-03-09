00e75c33bfefd1d0f37f2a656e60185da6bcfa0b
Novikova.Anna <nna.novikova19912020@gmail.com>
2023-03-09 23:10:22 +0300

    Лаб 3

---
+++ Сортировки/Лабораторная 3/Сортировка подсчетами/task.py
@@ -0,0 +1,27 @@
+from typing import Sequence
+
+
+def sort(container: Sequence[int]) -> Sequence[int]:
+    """
+    Сортировка подсчетами
+
+    1. Определите максимальное значение в массиве и заполните вспомогательный массив с подсчетом количества элементов.
+    2. Посчитайте количество каждого объекта
+    3. Зная количество каждого объекта, восстановите отсортированный массив
+
+    :param container: Массив, который надо отсортировать
+    :return: Отсортированный в порядке возрастания массив
+    """
+    if not container:
+        return container
+    max_value = max(container)
+    counts = [0] * (max_value + 1)
+    for value in container:
+        counts[value] += 1
+    result = []
+    for value in range(max_value + 1):
+        result.extend([value] * counts[value])
+    return result
+
+
+
